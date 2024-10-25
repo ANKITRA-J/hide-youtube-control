ced0ebc4fdb5e8c28ab3aded4da76c3c8d6545e8
Ankit Rajput <136639748+ANKITRA-J@users.noreply.github.com>
2024-10-26 01:01:02 +0530

    Update content.js
    will help removing the hover animation

--- hide youtube control/content.js
+++ hide youtube control/content.js
@@ -4,13 +4,9 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "toggleControls") {
     controlsHidden = !controlsHidden;
     if (controlsHidden) {
-      document.querySelectorAll('.ytp-chrome-bottom, .ytp-chrome-top').forEach(function (control) {
-        control.style.display = 'none';
-      });
+      document.body.classList.add('hidden-controls');
     } else {
-      document.querySelectorAll('.ytp-chrome-bottom, .ytp-chrome-top').forEach(function (control) {
-        control.style.display = '';
-      });
+      document.body.classList.remove('hidden-controls');
     }
   }
 });
